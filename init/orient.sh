61762bf3672eb98c1c68009dd1e2f5b8be99a2e8
Mohammad Naghavi <mohamnag@gmail.com>
2015-09-01 17:27:21 +0200

    minor changes

--- init/orient.sh
+++ init/orient.sh
@@ -3,7 +3,7 @@ EC2_IP=`curl -s http://169.254.169.254/latest/meta-data/local-ipv4`
 
 EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
 
-EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'`"
+EC2_REGION="`echo \"${EC2_AVAIL_ZONE}\" | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'`"
 
 EC2_SEC_GROUP=`curl -s http://169.254.169.254/latest/meta-data/security-groups`
 
