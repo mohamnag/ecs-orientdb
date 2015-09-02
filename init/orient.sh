dfcec30f4972a20efea32bda6a6157e033db4e66
Mohammad Naghavi <mohamnag@gmail.com>
2015-09-02 13:28:34 +0200

    fixing command in init script

--- init/orient.sh
+++ init/orient.sh
@@ -17,11 +17,10 @@ sed -i "s|ORIENTDB_ROOT_PASSWORD|${ORIENTDB_ROOT_PASSWORD}|" ${ORIENTDB_HOME}/co
 ${ORIENTDB_HOME}/bin/server.sh \
     -Xmx${MEM_LIMIT} \
     -Dhazelcast.ip=${EC2_IP} \
-	-Dhazelcast.access=${AWS_ACCESS_KEY} \
-	-Dhazelcast.secret=${AWS_SECRET_KEY} \
-	-Dhazelcast.region=${EC2_REGION} \
-	-Dhazelcast.group=${EC2_SEC_GROUP} \
-	-Dhazelcast.tag_key=${EC2_TAG_KEY} \
-	-Dhazelcast.tag_value=${EC2_TAG_VAL} \
-    -Ddistributed=true  $* \
-    /
+    -Dhazelcast.access=${AWS_ACCESS_KEY} \
+    -Dhazelcast.secret=${AWS_SECRET_KEY} \
+    -Dhazelcast.region=${EC2_REGION} \
+    -Dhazelcast.group=${EC2_SEC_GROUP} \
+    -Dhazelcast.tag_key=${EC2_TAG_KEY} \
+    -Dhazelcast.tag_value=${EC2_TAG_VAL} \
+    -Ddistributed=true  $*
